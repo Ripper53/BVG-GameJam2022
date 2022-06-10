f483dd912c0f40abbe0500fd4ca29fa4fa8d5952
MWellsBVG <mwells@bigvikinggames.com>
2022-06-10 19:07:04 -0400

    Fixed Naming Convention
    Update naming to Pascal / Camel Case

--- Assets/Scripts/Manger/SplashScreenManager.cs
+++ Assets/Scripts/Manger/SplashScreenManager.cs
@@ -7,36 +7,36 @@ using UnityEngine.SceneManagement;
 public class SplashScreenManager : MonoBehaviour
 {
     //Blackscreen for fade in fade out
-    public Image blackScreen;
+    public Image BlackScreen;
 
     //Colour used for the screen fade
     private Color colour;
 
     //Images to be displayed
     public GameObject BVGLogoImage;
-    public GameObject backgroundNameImage;
-    public GameObject gameNameImage;
+    public GameObject BackgroundNameImage;
+    public GameObject GameNameImage;
 
     //Fade rate
-    public float fadeRate;
+    public float FadeRate;
 
     //Timer
     private float timer;
     
     //Name of the main menu level to be loaded
-    public string mainMenu;
+    public string MainMenu;
 
     // Start is called before the first frame update
     void Start()
     {
         //Start with a black screen
-        colour = blackScreen.color;
+        colour = BlackScreen.color;
         colour.a = 1;
-        blackScreen.color = colour;
+        BlackScreen.color = colour;
         
-        //Show the TOJam first  
+        //Show the BVG Logo First 
         ShowGameBackgroundImage(false);
-        ShowTOJamOfficial(true);
+        ShowBVGLogoImage(true);
         ShowGameNameImage(false);
 
     }
@@ -49,7 +49,7 @@ public class SplashScreenManager : MonoBehaviour
 
         if(timer > 12f)
         {
-            SceneManager.LoadScene(mainMenu);
+            SceneManager.LoadScene(MainMenu);
         }
         else if(timer > 8f)
         {
@@ -57,38 +57,38 @@ public class SplashScreenManager : MonoBehaviour
         }
         else if(timer > 6f)
         {
-            colour.a -= fadeRate * 2.0f;
-            blackScreen.color = colour;
+            colour.a -= FadeRate * 2.0f;
+            BlackScreen.color = colour;
 
-            if(backgroundNameImage.activeSelf == false)
+            if(BackgroundNameImage.activeSelf == false)
             {
                 ShowGameBackgroundImage(true);
-                ShowTOJamOfficial(false);
+                ShowBVGLogoImage(false);
             }
         }
         else if(timer > 3f)
         {
-            colour.a += fadeRate;
-            blackScreen.color = colour;
+            colour.a += FadeRate;
+            BlackScreen.color = colour;
         }   
         else if (timer > 0f)
         {
-            colour.a -= fadeRate;
-            blackScreen.color = colour;
+            colour.a -= FadeRate;
+            BlackScreen.color = colour;
         }
     }
 
 
     void ShowGameBackgroundImage(bool show)
     {
-        backgroundNameImage.SetActive(show);
+        BackgroundNameImage.SetActive(show);
     }
-    void ShowTOJamOfficial(bool show)
+    void ShowBVGLogoImage(bool show)
     {
         BVGLogoImage.SetActive(show);
     }
     void ShowGameNameImage(bool show)
     {
-        gameNameImage.SetActive(show);
+        GameNameImage.SetActive(show);
     }
 }
