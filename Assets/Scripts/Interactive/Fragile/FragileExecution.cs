b02ef81301f757b07725433bcdbda40ececd84d7
Albar <GalacticHyperStar@gmail.com>
2022-06-12 13:16:46 -0400

    Droppable distractions!

--- Assets/Scripts/Interactive/Fragile/FragileExecution.cs
+++ Assets/Scripts/Interactive/Fragile/FragileExecution.cs
@@ -3,4 +3,5 @@ using UnityEngine;
 public abstract class FragileExecution : MonoBehaviour {
     public abstract bool InDanger { get; }
     public abstract void Frail(Vector2 velocity);
+    public abstract void EmitNoise();
 }
